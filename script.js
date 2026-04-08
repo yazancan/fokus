1a86e144780c3626c0f675973475959e3ad6fb04
Yasmin Zancan <yazancan@gmail.com>
2026-04-08 15:02:52 -0300

    Adiciona temporizador

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const html = document.querySelector("html");
-const displayTempo = document.querySelector("#timer");
 const banner = document.querySelector(".app__image");
 const titulo = document.querySelector(".app__title");
 
 const botoes = document.querySelectorAll(".app__card-button");
-const botaoIniciar = document.querySelector(".app__card-primary-button");
 const focoBt = document.querySelector(".app__card-button--foco");
 const curtoBt = document.querySelector(".app__card-button--curto");
 const longoBt = document.querySelector(".app__card-button--longo");
+const startPauseBt = document.querySelector("#start-pause");
 
-const musicaFocoInput = document.querySelector('#alternar-musica');
-const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const musicaFocoInput = document.querySelector("#alternar-musica");
+const musica = new Audio("/sons/luna-rise-part-one.mp3");
+const musicaPlay = new Audio("/sons/play.wav");
+const musicaPause = new Audio("/sons/pause.mp3");
+const musicaZero = new Audio("/sons/beep.mp3");
 
-const duracaoFoco = 1500;
-const duracaoDescansoCurto = 300;
-const duracaoDescansoLongo = 900;
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
 
 focoBt.addEventListener("click", () => {
   alterarContexto("foco");
@@ -32,9 +33,9 @@ longoBt.addEventListener("click", () => {
 });
 
 function alterarContexto(contexto) {
-  botoes.forEach (function(contexto){
-    contexto.classList.remove('active')
-  })
+  botoes.forEach(function (contexto) {
+    contexto.classList.remove("active");
+  });
   html.setAttribute("data-contexto", contexto);
   banner.setAttribute("src", `/imagens/${contexto}.png`);
 
@@ -59,11 +60,39 @@ function alterarContexto(contexto) {
   }
 }
 
-musica.loop = true
-musicaFocoInput.addEventListener('change', () => {
-  if(musica.paused) {
-    musica.play()
+musica.loop = true;
+musicaFocoInput.addEventListener("change", () => {
+  if (musica.paused) {
+    musica.play();
   } else {
-    musica.pause()
+    musica.pause();
   }
-})
+});
+
+const contagemRegressiva = () => {
+  if (tempoDecorridoEmSegundos <= 0) {
+    zerar();
+    musicaZero.play();
+    alert("tempo finalizado");
+    return;
+  }
+  tempoDecorridoEmSegundos -= 1;
+  console.log("temporizador", tempoDecorridoEmSegundos);
+};
+
+startPauseBt.addEventListener("click", iniciarOuPausar);
+
+function iniciarOuPausar() {
+  if (intervaloId) {
+    zerar();
+    musicaPause.play();
+    return;
+  }
+  musicaPlay.play();
+  intervaloId = setInterval(contagemRegressiva, 1000);
+}
+
+function zerar() {
+  clearInterval(intervaloId);
+  intervaloId = null;
+}
